7e9f8d2042dda2dbb9d8fdbb072130e68e390b96
Bartlomie Magari Biernat <123biernat123@gmail.com>
2022-01-24 12:36:48 +0100

    .

--- simplewires/pages/_app.js
+++ simplewires/pages/_app.js
@@ -2,8 +2,12 @@ import "../styles/globals.scss"
 import "../styles/normalize.scss"
 import "../styles/sassStyles/_typography.scss"
 import "../styles/sassStyles/_variables.scss"
+import smoothscroll from "smoothscroll-polyfill"
 
 function MyApp({ Component, pageProps }) {
+  if (typeof window !== "undefined") {
+    smoothscroll.polyfill()
+  }
   return <Component {...pageProps} />
 }
 
